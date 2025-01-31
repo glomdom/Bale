5fb126e14a5ec1ba6e0d407754f6d0b4fa883375
glomdom <22115@uktc-bg.com>
2025-01-31 19:46:35 +0200

    ref: local variable

--- Bale.Bindings/Vulkan/VulkanApp.cs
+++ Bale.Bindings/Vulkan/VulkanApp.cs
@@ -12,15 +12,15 @@ public sealed class VulkanApp : IDisposable {
     private readonly GLFWWindow _window;
     private readonly VulkanInstance _vulkanInstance;
     private readonly VulkanSurfaceManager _vulkanSurfaceManager;
-    private readonly VulkanPhysicalDeviceSelector _vulkanPhysicalDeviceSelector;
     private readonly VulkanLogicalDeviceManager _vulkanLogicalDeviceManager;
 
     public VulkanApp(string appName, int width, int height) {
         _window = new GLFWWindow(width, height, appName);
         _vulkanInstance = new VulkanInstance(appName, new Version(0, 0, 1));
         _vulkanSurfaceManager = new VulkanSurfaceManager(_vulkanInstance.Handle, _window);
-        _vulkanPhysicalDeviceSelector = new VulkanPhysicalDeviceSelector(_vulkanInstance.Handle, _vulkanSurfaceManager.Surface);
-        _vulkanLogicalDeviceManager = new VulkanLogicalDeviceManager(_vulkanPhysicalDeviceSelector.PhysicalDevice, _vulkanSurfaceManager.Surface);
+
+        var vulkanPhysicalDeviceSelector = new VulkanPhysicalDeviceSelector(_vulkanInstance.Handle, _vulkanSurfaceManager.Surface);
+        _vulkanLogicalDeviceManager = new VulkanLogicalDeviceManager(vulkanPhysicalDeviceSelector.PhysicalDevice, _vulkanSurfaceManager.Surface);
     }
     
     public void Run() {
