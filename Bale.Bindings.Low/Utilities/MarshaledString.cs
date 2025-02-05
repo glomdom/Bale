718e6502aec9ee1a59ed126aa619966840500309
glomdom <22115@uktc-bg.com>
2025-02-05 02:32:03 +0200

    perf: improve `MarshaledString`

--- Bale.Bindings.Low/Utilities/MarshaledString.cs
+++ Bale.Bindings.Low/Utilities/MarshaledString.cs
@@ -1,21 +1,36 @@
-using System.Runtime.InteropServices;
-using static Bale.Bindings.Common;
+using System.Text;
 
 namespace Bale.Bindings.Utilities;
 
 public sealed class MarshaledString : IDisposable {
-    private IntPtr _ptr;
+    private SafeHGlobalHandle? _handle;
 
     public MarshaledString(string str) {
-        _ptr = Marshal.StringToHGlobalAnsi(str);
+        ArgumentNullException.ThrowIfNull(str);
+
+        var bytes = Encoding.ASCII.GetBytes(str);
+        var byteCount = bytes.Length + 1; // +1 for \0
+
+        _handle = new SafeHGlobalHandle(byteCount);
+        unsafe {
+            var destination = new Span<byte>(_handle.DangerousGetHandle().ToPointer(), byteCount);
+            bytes.CopyTo(destination);
+            destination[bytes.Length] = 0; // \0
+        }
     }
 
-    public static implicit operator IntPtr(MarshaledString ms) => ms._ptr;
+    ~MarshaledString() {
+        Dispose();
+    }
+
+    public static implicit operator IntPtr(MarshaledString ms) => ms._handle?.DangerousGetHandle() ?? IntPtr.Zero;
     
     public void Dispose() {
-        if (_ptr == NULL) return;
+        if (_handle is null || _handle.IsClosed) return;
 
-        Marshal.FreeHGlobal(_ptr);
-        _ptr = NULL;
+        _handle.Dispose();
+        _handle = null;
+
+        GC.SuppressFinalize(this);
     }
 }
